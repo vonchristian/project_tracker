b72ce081047142c876ad27e97fa12769fab4d16b
Von Christian Halip <vc.halip@gmail.com>
2016-04-29 19:57:03 +0800

    Inventory accepts nested attributes for stocks.

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,7 @@
 class Inventory < ApplicationRecord
+  has_many :stocks, class_name: "Inventories::Stock"
   validates :description, :unit_price, presence: true
   validates :unit_price, numericality:{ greater_than: 0.1 }
+
+  accepts_nested_attributes_for :stocks
 end
--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -2,12 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Inventory, :type => :model do
   describe "associations" do
+    it { is_expected.to have_many :stocks }
   end
   describe "validations" do
     it { is_expected.to validate_presence_of :description }
     it { is_expected.to validate_presence_of :unit_price }
     it do
-      is_expected.to validate_numericality_of(:unit_price).is_greater_than(0.1)  
+      is_expected.to validate_numericality_of(:unit_price).is_greater_than(0.1)
     end
   end
+
+  describe "nested attributes" do
+     it { is_expected.to accept_nested_attributes_for(:stocks) }
+   end
 end
