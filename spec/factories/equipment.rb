a8e3479aecd95ea08d1c8da7497870a2e6aea6c4
Von Christian Halip <vc.halip@gmail.com>
2016-04-29 22:03:53 +0800

    Editing an equipment feature.:

--- spec/factories/equipment.rb
+++ spec/factories/equipment.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     plate_number "ABC 233"
     make_and_model "Isuzu DMAXX 4x4"
     acquisition_cost 1_650_500
+    acquisition_date '2016/02/21'
     status 1
   end
 end
---
+++ spec/features/equipment/editing equipment_feature_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'Editing an equipment', type: :feature do
+  #  before (:each) do
+  #    user = FactoryGirl.create(:user)
+  #    login_as(user, :scope => :user)
+  #  end
+
+   given! (:equipment) { create(:equipment) }
+  scenario 'with valid information' do
+    visit equipment_path(equipment)
+    click_link "Update"
+    fill_in "Make and model", with: "DMAXX"
+    fill_in "Plate number", with: "ABC 123"
+    fill_in "Acquisition cost", with: 1_000_000
+    fill_in "Acquisition date", with: "2016/05/02"
+    click_button "Save Equipment"
+
+    expect(page).to have_content("updated successfully.")
+    expect(page).to have_content("DMAXX")
+  end
+  end
