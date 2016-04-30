019c1700606b6767dcee505516c57d54737f5552
Von Christian Halip <vc.halip@gmail.com>
2016-04-30 18:11:40 +0800

    Add has_many contracts to contractor model.

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -1,4 +1,6 @@
 class Contractor < ApplicationRecord
+  has_many :contracts
+  has_many :projects, through: :contracts
   validates :first_name, :last_name, :company, :position, presence: true
 
   def full_name
--- spec/models/contractor_spec.rb
+++ spec/models/contractor_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Contractor, :type => :model do
   describe "associations" do
+    it {is_expected.to have_many :contracts }
+    it {is_expected.to have_many :projects }
 
   end
 
